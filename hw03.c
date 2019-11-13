aa1dce4d1cd0dc26ff77278e9286b20572d73841
Ariana-N <ariana.nieves@cooper.edu>
2019-11-13 18:12:12 -0500

    complete

--- hw03.c
+++ hw03.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef enum {
+typedef enum {//assigns names to integral constants
   NAME_ONLY = 0,
   MAJOR_AND_NAME = 1, 
-  YEAR_AND_NAME = 2
+  YEAR_AND_NAME = 2,
 } Mode;
 
 typedef struct {
   char *first_name;
   char *last_name;
-  // TODO: add fields here for major and year
+  char *major;
+  int year;
 } Student;
 
 void print_student(Mode m, Student s) {
@@ -17,27 +19,34 @@ void print_student(Mode m, Student s) {
     case NAME_ONLY:
       printf("%s %s\n", s.first_name, s.last_name);
       break;
-  // TODO: handle other cases
+    case MAJOR_AND_NAME:
+      printf("%s %s %s\n", s.major, s.first_name, s.last_name);
+      break;
+    case YEAR_AND_NAME:
+      printf("%d %s %s\n", s.year, s.first_name, s.last_name);
+      break;
+
   }
 }
 
-/*
-argv[1] will be the Mode integer.
-argv[2]-argv[5] will be student info in the form [first_name, last_name, major, year].
-argv[6]-argv[9], if present will be student info for a second student in the same form, and so on.
-You will need to check argc to determine the number of students passed at runtime.
-The fields are all strings (char[]), except year is a number (int)
-*/
 int main(int argc, char **argv) {
-  // TODO: parse argv to populate student structs 
-  // for now, here's two hardcoded students:
-  Student s1, s2;
-  s1.first_name = "Julia";
-  s1.last_name = "A";
-  s2.first_name = "Peter";
-  s2.last_name = "Cooper";
-  
-  print_student(NAME_ONLY, s1);
-  print_student(NAME_ONLY, s2);
+
+int mode;  
+mode = atoi(argv[1]);
+
+int x=2;
+
+while(x < argc)
+{ 
+
+  Student s;
+  s.first_name = argv[x];       //argv[2]
+  s.last_name = argv[x+1];      //argv[3]
+  s.major = argv[x+2];          //argv[4]
+  s.year = atoi(argv[x+3]);     //argv[5]
+  print_student(mode, s);
+  x+=4;                         //+= incriment the variable on the left by the value on the right
+}
   return 0;
+
 }
